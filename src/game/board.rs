83ca26647a88f05bc13c99542fecc81e8a58775d
abomb4 <yangrlx@gmail.com>
2018-07-25 14:41:00 +0800

    Submit some code

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,30 +1,88 @@
-const BOARD_SIZE: usize = 19;
+const DEFAULT_BOARD_SIZE: usize = 19;
+
+pub(in game) type BoardPoint = usize;
 
 /// A Gomoku game board
 pub(in game) struct Board {
-    board: [[usize; BOARD_SIZE]; BOARD_SIZE]
+    board: [[usize; DEFAULT_BOARD_SIZE]; DEFAULT_BOARD_SIZE],
+    size: usize,
 }
 
-pub const BOARD_NONE: usize = 0;
-pub const BOARD_WHITE: usize = 1;
-pub const BOARD_BLACK: usize = 2;
+pub const BOARD_EMPTY: BoardPoint = 0;
+pub const BOARD_WHITE: BoardPoint = 1;
+pub const BOARD_BLACK: BoardPoint = 2;
+
+fn translate_board_point(target: BoardPoint) -> &'static str {
+    match target {
+        BOARD_EMPTY => "Empty",
+        BOARD_WHITE => "White",
+        BOARD_BLACK => "Black",
+        _ => panic!("Unknown board point {}.", target.clone())
+    }
+}
 
 impl Board {
     /// Create new empty game board
     pub fn new() -> Board {
-        let board: [[usize; BOARD_SIZE]; BOARD_SIZE] = [[BOARD_NONE; BOARD_SIZE]; BOARD_SIZE];
+        const SIZE: usize = DEFAULT_BOARD_SIZE;
+        let board: [[usize; SIZE]; SIZE] = [[BOARD_EMPTY; SIZE]; SIZE];
 
-        return Board { board };
+        return Board { board, size: SIZE };
     }
 
     /// Draw game board to console
     pub fn draw(&self) {
-        for i in 0..BOARD_SIZE {
-            for j in 0..BOARD_SIZE {
+        for i in 0..self.size {
+            for j in 0..self.size {
                 print!("{}", self.get_board_symbol(i, j));
             }
             println!();
         }
+    }
+
+    /// Get a point from board
+    ///
+    /// x and y starts by 1, not 0
+    pub fn get(&self, x: usize, y: usize) -> Result<BoardPoint, String> {
+        if !self.point_range_check(x, y) {
+            return Err(format!("Coordinate ({}, {}) is out of bound.", x, y));
+        }
+
+        let i = x - 1;
+        let j = y - 1;
+
+        Ok(self.board[i][j])
+    }
+
+    /// Place a piece to board
+    pub fn place(&mut self, x: usize, y: usize, point: BoardPoint) -> Result<BoardPoint, String> {
+        let current_point = match self.get(x, y) {
+            Ok(ok) => ok,
+            Err(e) => return Err(e)
+        };
+
+        if current_point != BOARD_EMPTY {
+            return Err(format!("Coordinate ({}, {}) is {}, not empty.",x, y, translate_board_point(current_point)))
+        }
+
+        let i = x - 1;
+        let j = y - 1;
+
+        self.board[i][j] = point;
+        Ok(point)
+    }
+
+    /// Check the range of x and y is valid
+    fn point_range_check(&self, x: usize, y: usize) -> bool {
+        if x >= self.size || x <= 0 {
+            return false;
+        }
+
+        if y >= self.size || y <= 0 {
+            return false;
+        }
+
+        return true;
     }
 
     ///
@@ -32,11 +90,11 @@ impl Board {
     ///
     fn get_board_symbol(&self, i: usize, j: usize) -> &str {
         let data = self.board[i][j];
-        let max_index = BOARD_SIZE - 1;
+        let max_index = self.size - 1;
         match data {
             BOARD_WHITE => "○",
             BOARD_BLACK => "●",
-            BOARD_NONE => if i == 0 && j == 0 { "┌" } else if i == 0 && j == max_index { "┐" } else if i == max_index && j == 0 { "└" } else if i == max_index && j == max_index { "┘" } else if i == 0 { "┬" } else if i == max_index { "┴" } else if j == 0 { "├" } else if j == max_index { "┤" } else { "┼" }
+            BOARD_EMPTY => if i == 0 && j == 0 { "┌" } else if i == 0 && j == max_index { "┐" } else if i == max_index && j == 0 { "└" } else if i == max_index && j == max_index { "┘" } else if i == 0 { "┬" } else if i == max_index { "┴" } else if j == 0 { "├" } else if j == max_index { "┤" } else { "┼" }
             _ => panic!("Unknown board data detected.")
         }
     }
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,38 +2,83 @@ use game::board::Board;
 
 mod board;
 
+type Player = u8;
+
 /// The two Gomoku player
-pub enum Player {
-    BLACK,
-    WHITE
+const BLACK: Player = 0;
+const WHITE: Player = 1;
+
+fn translate_to_board_type(p: Player) -> board::BoardPoint {
+    match p {
+        BLACK => board::BOARD_BLACK,
+        WHITE => board::BOARD_WHITE,
+        _ => panic!(format!("Invalid player {}.", p))
+    }
 }
 
 ///
 /// A Gomoku game instance.
 ///
 pub struct Game {
-    board: Board
+    board: Board,
+    current_player: u8,
+    history: Vec<(Player, usize, usize)>,
 }
 
 impl Game {
-
-    /// Create a new game
-    pub fn new() -> Game{
-        Game { board: Board::new() }
+    /// Create a new game with black first
+    pub fn new() -> Game {
+        Game {
+            board: Board::new(),
+            current_player: BLACK,
+            history: vec![]
+        }
     }
 
     /// Initialize the game
-    pub fn init(&self) {
+    pub fn init(&mut self) {
         self.board.draw()
     }
 
     /// Start the game!
-    pub fn start(&self) {}
+    pub fn start(&mut self) {}
 
     /// Place a piece in the game
     ///
     /// Returns the winner if the game is end.
-    pub fn point(&self, x: usize, y: usize) -> Option<Player> {
-        // TODO place the piece to board, and check the game is end
+    pub fn point(&mut self, x: usize, y: usize) -> Result<Option<Player>, String> {
+        // place the piece to board, and check the game is end
+        let place = self.board.place(x, y, translate_to_board_type(self.current_player));
+        if place.is_err() {
+            return Err(place.unwrap_err())
+        }
+
+        let winner = if self.check_game_end() {
+            Some(self.current_player)
+        } else {
+            None
+        };
+
+        self.current_player = match self.current_player {
+            BLACK => WHITE,
+            WHITE => BLACK,
+            _ => panic!(format!("Invalid player {}.", self.current_player))
+        };
+
+        Ok(winner)
+    }
+
+    /// Check the game is end, if end, returns true; not end the return false.
+    fn check_game_end(&self) -> bool {
+        let last_point = match self.history.last() {
+            Some(a) => a,
+            None => return false
+        };
+
+        match last_point.0 {
+            BLACK => true,
+            _ => false
+        }
+
     }
 }
